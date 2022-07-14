80feaeea6745ef22e99d78385d708c76bc79ab56
Ahmed Gagan <67264609+aahhmmeedd-a@users.noreply.github.com>
2022-07-14 18:42:50 +0530

    planetscale + prisma final implementation

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,41 @@
 import * as React from 'react';
 
-//import prisma client
+//import data.json file
+import courseData from '../public/data.json';
 
 //Component that renders the course card
 import CourseCard from './coursecard';
 //Component that renders the navbar
 import NavBar from './navbar';
 
-import prisma from '../lib/prisma.js';
+import {Prisma, PrismaClient} from '@prisma/client';
 
+let prisma = new PrismaClient();
 
 // index.tsx
 export async function getStaticProps() {
-    const feed = await prisma.course.findMany();
-    return {
-      props: { feed },
-    };
+  const feed = await prisma.course.findMany();
+  return {
+    props: { feed },
+  };
 };
 
-export default function Home({ feed }) {
+//for (let i = 0; i< courseData.length; i++) {
+  //let createMany = await prisma.course.create({
+    //data: {
+      //id: 'id' + i,
+      //data: courseData[i]
+    //},
+  //})
+//}
 
+export default function Home({ feed }) {
   return (
     <>
       <div className="card-container">
         <font color="white">
           <NavBar />
+          {console.log(feed)}
           {
             feed.map((feeds, index) => (
               <CourseCard
